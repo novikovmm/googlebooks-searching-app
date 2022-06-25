7897bc64c3efa01260a489c14c2053fb2fe02ebc
Михаил Новиков <mikhail@MacBook-Air-Mikhail.local>
2022-06-25 22:46:33 +0300

    small changes

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -13,28 +13,6 @@ const BooksContainer = () => {
 	}
 
 	return (
-		// <>
-		// 	{total > 0 ? (
-		// 		<h5 className='text-center p-3'>Found {total} books</h5>
-		// 	) : (
-		// 		<h5 className='text-center p-3'> Start books searching</h5>
-		// 	)}
-		// 	<div className='container mt-2'>
-		// 		<div className='row g-3'>
-		// 			{books?.length !== 0 && books?.map((book) => <Book key={book.id + '+' + book.etag} {...book} />)}
-		// 		</div>
-		// 		<div className='d-grid gap-2 col-6 mx-auto'>
-		// 			{total > 0 && (
-		// 				<button
-		// 					onClick={() => load(books.length)}
-		// 					type='button'
-		// 					className='btn btn-outline-secondary btn-lg mx-auto my-4'>
-		// 					load more...
-		// 				</button>
-		// 			)}
-		// 		</div>
-		// 	</div>
-		// </>
 		<>
 			{total > 0 ? (
 				<h5 className='text-center p-3'>Found {total} books</h5>
